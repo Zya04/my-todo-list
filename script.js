2f6f37a27e09e65329e6099fa1c4dc391e67863d
Neil Richter <me@neilrichter.com>
2017-11-29 14:24:59 +0100

    Added the "Add Category" feature

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 window.onload = function() {
     var board = document.querySelector('#board');
-}
+    var model = document.querySelectorAll('#model');
+    
+
+    document.querySelector('#addCategory').onclick = function() {
+        board.appendChild(model[0].cloneNode(true));
+        model = document.querySelectorAll('#model');
+        model[1].classList.remove('hidden');
+        model[1].removeAttribute("id");
+    };
+};
