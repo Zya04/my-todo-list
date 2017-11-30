3d4de591a49ee35526c4616d8d363a475dcb9d33
Neil Richter <me@neilrichter.com>
2017-11-30 22:14:58 +0100

    Return to the line of the button "Add category" when starting on a new line fixed

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 window.onload = function() {
     var board = document.querySelector('#board');
     var model = document.querySelectorAll('#model');
-    
+
+    //creating the block, didn't use .cloneNode()
+    function newCategory() {
+        var newTask = document.createElement('div');
+        newTask.className='newTask';
+        newTask.appendChild(document.createElement('p'));
+        newTask.querySelector('p').innerHTML = 'New Task';
+        newTask.appendChild(document.createElement('i'));
+        newTask.querySelector('i').className = 'fa fa-plus';
+        var newCategory = document.createElement('div');
+        newCategory.className = 'category';
+        newCategory.appendChild(newTask);
+        return newCategory;
+    }
 
     document.querySelector('#addCategory').onclick = function() {
-        board.appendChild(model[0].cloneNode(true));
-        model = document.querySelectorAll('#model');
-        model[1].classList.remove('hidden');
-        model[1].removeAttribute("id");
+        board.insertBefore(newCategory(), addCategory);
     };
 };
