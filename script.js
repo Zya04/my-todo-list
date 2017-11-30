31bf24ffe7de8022b2d4937b4cb18f2eae91ba80
Neil Richter <me@neilrichter.com>
2017-11-30 22:56:09 +0100

    Now possible to add more tasks to the first category. Working on the others.

--- script.js
+++ script.js
@@ -10,11 +10,25 @@ window.onload = function() {
         addTask.querySelector('i').className = 'fa fa-plus';
         var newCategory = document.createElement('div');
         newCategory.className = 'category';
-        newCategory.appendChild(newTask);
+        newCategory.appendChild(addTask);
         return newCategory;
+    }
+
+    function newTask() {
+        var newTask = document.createElement('div');
+        newTask.className = 'task';
+        newTask.appendChild(document.createElement('h3'));
+        newTask.querySelector('h3').innerHTML = 'Task title';
+        newTask.appendChild(document.createElement('p'));
+        newTask.querySelector('p').innerHTML = 'Add description';
+        return newTask;
     }
 
     document.querySelector('#addCategory').onclick = function() {
         board.insertBefore(newCategory(), this);
+    };
+
+    document.querySelector('.addTask').onclick = function() {
+        this.parentElement.appendChild(newTask());
     };
 };
